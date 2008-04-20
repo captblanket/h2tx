d40e904e4b13ef7f1ac230eea61f9ed29bc6be5f
= <captblanket@gmail.com>
2008-04-20 02:54:51 +0200

    builder koristi eksterni template

--- builder.rb
+++ builder.rb
@@ -1,14 +1,15 @@
 require "getter"
 require "parser"
+require "erb"
 
 # simulacije gettera
 # uris_to_parse = ["input_test_2000.htm", "input_test_2001.htm", "input_test_2002.htm", "input_test_2003.htm", "input_test_2004.htm", \
 #                 "input_test_2005.htm", "input_test_2006.htm", "input_test_2007.htm", "input_test_2008.htm"]
 # uris_to_parse = deserialize(URIS_FILE)
-# uris_to_parse = ["input_test_2007-i.htm"]
-uris_to_parse = ["http://www.nn.hr/clanci/sluzbeno/2006/0873.htm"]
+# uris_to_parse = ["input_test_2000.htm"]
+uris_to_parse = ["http://www.nn.hr/clanci/sluzbeno/2007/0360.htm"]
 
-puts "Počinjem parsiranje\n"
+puts "Počinjem parsiranje"
 uris_to_parse.each do |source|
   
   # prvo dobij ID i godinu (korisno za hvatanje grešaka)
@@ -21,87 +22,34 @@ uris_to_parse.each do |source|
   begin
     loaded_html = open(source) { |line| line.read }
   rescue SocketError => e
-    puts "\nNe mogu se spojiti na poslužitelj Narodnih novina.\n"
+    puts "\nNe mogu se spojiti na poslužitelj Narodnih novina."
     break
   rescue OpenURI::HTTPError => e
-    dump_error("#{@year}/#{@id}: traženi dokument ne postoji\n")
+    dump_error("traženi dokument ne postoji")
     next
   end
-  
+
   # konverzija i čišćenje
   html_utf8 = convert_to_utf8(loaded_html)
   xml = tidy_html(html_utf8) 
   tei_xml = conform_to_tei(xml)[0]
   
-  tei_header = %{<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE TEI.2 PUBLIC "-//TEI P5//DTD Main Document Type//EN" "http://www.tei-c.org/Guidelines/DTD/tei2.dtd" [
-<!ENTITY % TEI.prose 'INCLUDE'>
-<!ENTITY % TEI.linking 'INCLUDE'>
-<!ENTITY % TEI.figures 'INCLUDE'>
-<!ENTITY % TEI.analysis 'INCLUDE'>
-<!ENTITY % TEI.XML 'INCLUDE'>
-<!ENTITY % ISOlat1 SYSTEM 'http://www.tei-c.org/Entity_Sets/Unicode/iso-lat1.ent'>
-%ISOlat1;
-<!ENTITY % ISOlat2 SYSTEM 'http://www.tei-c.org/Entity_Sets/Unicode/iso-lat2.ent'>
-%ISOlat2;
-<!ENTITY % ISOnum SYSTEM 'http://www.tei-c.org/Entity_Sets/Unicode/iso-num.ent'>
-%ISOnum;
-<!ENTITY % ISOpub SYSTEM 'http://www.tei-c.org/Entity_Sets/Unicode/iso-pub.ent'>
-%ISOpub;
-]>
-<TEI.2 lang="hr-HR">
-<teiHeader>
-<fileDesc>
-<titleStmt>
-<title>#{@title}</title>
-<author>#{@author}</author>
-</titleStmt>
-<editionStmt>
-<edition>
-<date>#{@date}</date>
-</edition>
-</editionStmt>
-<publicationStmt>
-<authority/>
-<address/>
-</publicationStmt>
-<sourceDesc>
-<p>Brankov diplomski</p>
-</sourceDesc>
-</fileDesc>
-<profileDesc>
-<langUsage default="NO">
-<language id="hr-HR">ISO hr-HR</language>
-</langUsage>
-</profileDesc>
-<revisionDesc>
-<change>
-<date/>
-<respStmt>
-<name/>
-</respStmt>
-<item>revision</item>
-</change>
-</revisionDesc>
-</teiHeader>
-<text>
-}
-  
-  # spajanje headera s pročišćenim XML-om
-  final_output = tei_header + tei_xml + "\n</text>\n</TEI.2>"
-  
-  # ovdje se dokument sprema lokalno
-  begin
-    open("output_test/#{@title.to_ascii}.xml", "w") { |line| line.puts final_output }  
-  rescue Errno::ENAMETOOLONG => e
-    dump_error("#{@year}/#{@id}: nije parsiran zbog predugog imena\n")
-    next
-  rescue Errno::ENOENT => e
-    dump_error("#{@year}/#{@id}: nije parsiran zbog ilegalnih znakova u imenu\n")
-    next
-  end
-  
+  # učitaj template i napravi supstitucije
+  tei_template = open("tei_template.rxml") { |line| line.read }
+  final_output = ERB.new(tei_template, nil, "", "final_output").result(binding)
+
   puts @title
   puts @date
   puts @author
+  
+  # spremi dokument lokalno
+  begin
+    open("#{@title.to_ascii}.xml", "w") { |line| line.puts final_output }
+  rescue Errno::ENAMETOOLONG => e
+    @title = @title.truncate
+    retry
+  rescue Errno::ENOENT => e
+    dump_error("nije spremljen zbog ilegalnih znakova u imenu")
+    next
+  end
 end
--- parser.rb
+++ parser.rb
@@ -5,7 +5,7 @@ require "iconv"
 Tidy.path = '/usr/lib/libtidy.dylib'
 
 def dump_error(error)
-  puts error
+  puts error = "#{@year}/#{@id}: " + error
   open("error_log.txt", "a") { |log| log.puts(error)}
 end
 
@@ -15,7 +15,8 @@ def convert_to_utf8(input)
   begin    
     converted = Iconv.new(output_encoding, input_encoding).iconv(input)  
   rescue Iconv::IllegalSequence => e
-    dump_error("#{@year}/#{@id}: problem s konverzijom u UTF-8\n")
+    puts e.message
+    dump_error("problem s konverzijom u UTF-8")
   end
 end
 
@@ -31,9 +32,9 @@ def conform_to_tei(input)
   tei = input
   tei.gsub!("&nbsp;", "")
   tei.gsub!(/<(\w+)\s+[^>]*>/, '<\1>') # makni sve atribute iz tagova
-  tei.gsub!(/<(table|style)>.*?<\/(table|style)>/m, '') # makni sve tablice i njihov sadržaj 
-  tei.gsub!(/<\/?(div|sup|html|head|u|span)>/, '') # makni div, sup, html, head, u ali ostavi sadržaj
-  tei.gsub!(/<(\/?)(h\d|center|font)>/, '<\1p>') # headinge i center zamijeni s paragrafima
+  tei.gsub!(/<(table|style)>.*?<\/(table|style)>/m, '') # makni tag i njegov sadržaj 
+  tei.gsub!(/<\/?(div|su[bp]|html|head|u|span|font|[uo]l|a)>/, '') # makni ali ostavi sadržaj
+  tei.gsub!(/<(\/?)(h\d|center|li|pre)>/, '<\1p>') # zamijeni s paragrafima
   tei.gsub!("<br>", "<lb/>")
   tei.gsub!("<b>", '<hi rend="bold">')
   tei.gsub!("<i>", '<hi rend="italic">')
@@ -42,7 +43,7 @@ def conform_to_tei(input)
   tei.gsub!(/<p>\s*?<\/p>/, "") # makni prazne paragrafe
   tei.gsub!("…", "...")
   tei.gsub!("\302\255", "") # čudan znak koji ne postoji?!
-  tei.gsub!(/(<body>\s*?<p>.*?<\/p>)\s*?<p>.*?<\/p>/m, '\1') # makni ID
+  tei.gsub!(/<!--.*?-->/m, "") # makni HTML komentare
   tei.gsub!(/\n(\n)+/, "\n") # makni sve newlineove osim prvog
   tei.squeeze!(" ")
   
@@ -54,7 +55,7 @@ def conform_to_tei(input)
   rescue NoMethodError => e
     @date = "___DATUM_NEPOZNAT___"
     @title = "___NAZIV_NEPOZNAT___"
-    dump_error("#{@year}/#{@id}: datum i naziv nepoznati\n")
+    dump_error("datum i naziv nepoznati")
   end
   
   # sad makni i sam title
@@ -63,18 +64,44 @@ def conform_to_tei(input)
   
   # izvuci autora
   begin
-    author_scan = tei.scan(%r{<body>\s*?<p>(.*?)</p>}).flatten
-    @author = author_scan[0].strip
+    author_scan = tei.scan(%r{<body>\s*?<p>(.*?)</p>}m).flatten
+    @author = author_scan[0].strip # TODO provjeri da autor ne sadrži <lb/>, a može biti i unutar <hi> npr. 2005/0781
+    raise NoMethodError if @author =~ /\d/ # provjeri da nije broj
   rescue NoMethodError => e
     @author = "___AUTOR_NEPOZNAT___"
-    dump_error("#{@year}/#{@id}: autor nepoznat\n")
+    dump_error("autor nepoznat")
+  end
+  
+  # makni ID; ako je autor nepoznat (vjerojatno se radi o ispravku),
+  # prvi p child bodyja će biti ID, u svim ostalim slučajevima je drugi
+  if @author == "___AUTOR_NEPOZNAT___"
+    tei.gsub!(/(<body>)\s*?<p>.*?<\/p>/m, '\1')
+  else
+    tei.gsub!(/(<body>\s*?<p>.*?<\/p>)\s*?<p>.*?<\/p>/m, '\1')
   end
   
   return tei, @date, @title, @author
 end
 
 public
-def to_ascii # http://www.jroller.com/obie/entry/fix_that_tranny_add_to
-  converter = Iconv.new('ASCII//IGNORE//TRANSLIT', 'UTF-8') 
-  converter.iconv(self).unpack('U*').select{ |cp| cp < 127 }.pack('U*')
+def to_ascii
+  self.gsub!(/š/, "s")
+  self.gsub!(/Š/, "S")
+  self.gsub!(/đ/, "dj")
+  self.gsub!(/Đ/, "Dj")
+  self.gsub!(/Dj([A-Z])/, 'DJ\1')
+  self.gsub!(/ž/, "z")
+  self.gsub!(/Ž/, "Z")
+  self.gsub!(/č/, "c")
+  self.gsub!(/Č/, "C")
+  self.gsub!(/ć/, "c")
+  self.gsub!(/Ć/, "C")
+  self.gsub!(/[<>|\\*"]/, "")
+  self.gsub!(/[:?]/, " ")
+  self.gsub!(/\//, "-")
+  self
+end
+
+def truncate
+  self[0..127]
 end
